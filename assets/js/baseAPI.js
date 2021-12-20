606dc98df956a865b0a96263a8da38799e101d02
Your Name <you@example.com>
2021-12-20 10:12:27 +0800

    完成了首页

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,4 +2,21 @@
 $.ajaxPrefilter((option) => {
     // 在发起真正的Ajax请求之前，统一拼接请求的根路径
     option.url = 'http://api-breakingnews-web.itheima.net' + option.url
+
+    // 统一为有权限的接口加headers请求头
+    if (option.url.indexOf('/my/') !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载complete回调函数
+    option.complete = (res) => {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 清空token
+            localStorage.removeItem('token')
+                // 强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,20 @@
 $(function() {
     // 获取用户基本信息
     getUserInfo();
+    var layer = layui.layer
+        // 实现退出功能
+    $('#logout').on('click', function() {
+        // 提示用户是否确认退出
+        layer.confirm('是否退出登录?', { icon: 3, title: '提示' }, (index) => {
+            // 清空本都存储的token
+            localStorage.removeItem('token')
+                // 跳转到登录页面
+            location.href = '/login.html'
+
+            // 关闭confirm询问框
+            layer.close(index)
+        })
+    })
 })
 
 // 获取用户的基本信息
@@ -8,9 +22,9 @@ function getUserInfo() {
     $.ajax({
         method: 'GET',
         url: '/my/userinfo',
-        headers: {
-            Authorization: localStorage.getItem('token') || ''
-        },
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
         success: function(res) {
             if (res.status !== 0) {
                 return layui.layer.msg(res.message, { icon: 2 })
@@ -18,7 +32,7 @@ function getUserInfo() {
 
             // 渲染用户头像
             $('.welcome').html('欢迎&nbsp;&nbsp;' + res.data.username || res.data.nickname)
-            var firstName = res.data.username.slice(0, 1) || res.data.nickname.slice(0, 1)
+            var firstName = res.data.username.slice(0, 1)
             $('.worduser').html(firstName.toUpperCase())
             if (res.data.user_pic !== null) {
                 $('.layui-nav-img').attr('src', res.data.user_pic).show();
@@ -27,6 +41,16 @@ function getUserInfo() {
                 $('.layui-nav-img').hide();
                 $('.worduser').show();
             }
-        }
+        },
+        // 无论成功还是失败，都会调用complete回调函数
+        // complete: function(res) {
+        //     // 在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //         // 强制清空token
+        //         localStorage.removeItem('token')
+        //             // 强制调转到登录页面
+        //         location.href = '/login.html'
+        //     }
+        // }
     })
 }
